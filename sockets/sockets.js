84633208682597f01e0e7b9fed9dbe29b4529f01
Erick Pinzon <erickpinzonhuerta@gmail.com>
2023-05-23 09:00:40 -0600

    Update sockets.js

--- sockets/sockets.js
+++ sockets/sockets.js
@@ -267,6 +267,24 @@ function socket(io) {
       }
     });
 
+    // Consultar Patrones Asignados
+    socket.on('cliente:consultarPatronesAsignados', async (clave_eje) => {
+      const pAsignados = await Afil.findAll({
+        where: {clave_eje : clave_eje}
+       });
+      socket.emit('servidor:patronesAsignados', pAsignados);
+    });
+
+    // Consultar patrones no asignados
+    socket.on('cliente:consultarPatronesNoAsignados', async (clave_eje) => {
+      const pNoAsignados = await Afil.findAll({
+        where: {
+          clave_eje: { [Op.not]: clave_eje }
+        }
+       });
+      socket.emit('servidor:patronesNoAsignados', pNoAsignados);
+    });
+
     // Maneja el evento de desconexión del cliente
     socket.on("disconnect", () => {
       // Programar eventos (innecesarios por ahora)
