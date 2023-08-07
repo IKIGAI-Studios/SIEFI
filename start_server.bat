7a7d9f74a19c0a2fb80b964f09b4b60f6dff0e9c
Erick Pinzon <erickph.ti21@utsjr.edu.mx>
2023-08-06 21:07:22 -0600

    package changes

--- start_server.bat
+++ start_server.bat
@@ -6,7 +6,5 @@ git fetch origin
 @REM Instalar todas las dependencias de node
 @REM ! Se cierra despues de que se instalan las dependencias
 @REM npm install
-@REM Abrir navegador con la pagina de inicio
-start "SIEFI" "http://localhost:3000"
 @REM Ejecutar servidor modo silencioso
 nodemon --quiet index
