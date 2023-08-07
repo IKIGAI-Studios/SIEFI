8f724f675445aaf3cdef1c56ac06686e8b9491be
Erick Pinzon <erickph.ti21@utsjr.edu.mx>
2023-08-06 21:15:05 -0600

    Update start_server.bat

--- start_server.bat
+++ start_server.bat
@@ -1,4 +1,6 @@
-@REM TODO: Investigar como hacer un fecht a git y luego ejecutar el servidor
+@REM Iniciar la consola minimizada y que no salgan los comandos
+@echo off
+@echo Iniciando servidor SIEFI ...
 @REM Entrar a ruta del proyecto
 cd C:\Users\erick\Documents\Github\testSiefi\SIEFI\
 @REM Actualizar repositorio de git
@@ -6,5 +8,7 @@ git fetch origin
 @REM Instalar todas las dependencias de node
 @REM ! Se cierra despues de que se instalan las dependencias
 @REM npm install
+@REM Limpiar la consola
+cls
 @REM Ejecutar servidor modo silencioso
 nodemon --quiet index
