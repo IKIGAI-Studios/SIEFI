f4efc0a1c0852626fd8fe5577def58358c16522d
Erick Pinzon <erickph.ti21@utsjr.edu.mx>
2023-08-06 21:16:21 -0600

    Update start_server.bat

--- start_server.bat
+++ start_server.bat
@@ -1,9 +1,9 @@
 @REM Iniciar la consola minimizada y que no salgan los comandos
 @echo off
 @echo Iniciando servidor SIEFI ...
-@REM Entrar a ruta del proyecto
+@REM Entrar a ruta del proyecto | Cambiar por la ruta del proyecto
 cd C:\Users\erick\Documents\Github\testSiefi\SIEFI\
-@REM Actualizar repositorio de git
+@REM Actualizar repositorio de git | Opcional
 git fetch origin
 @REM Instalar todas las dependencias de node
 @REM ! Se cierra despues de que se instalan las dependencias
