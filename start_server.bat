1fac476c4bfbdbf07f4aab56dba4702d7074ce65
Erick Pinzon <erickph.ti21@utsjr.edu.mx>
2023-08-06 19:59:33 -0600

    Update start_server.bat

--- start_server.bat
+++ start_server.bat
@@ -1,3 +1,4 @@
 @REM TODO: Investigar como hacer un fecht a git y luego ejecutar el servidor
-cd C:\Users\erick\Documents\Github\SIEFI\
+cd C:\Users\erick\Documents\Github\testSiefi\SIEFI\
+git fetch origin
 nodemon --quiet index
