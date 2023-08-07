c2c3fa4da15db36a3ec99e0bfc45825333ed64d1
Gabriela <gabrielasotozepeda821@gmail.com>
2023-08-07 09:47:50 -0600

    Update start_server.bat

--- start_server.bat
+++ start_server.bat
@@ -2,13 +2,13 @@
 @echo off
 @echo Iniciando servidor SIEFI ...
 @REM Entrar a ruta del proyecto | Cambiar por la ruta del proyecto
-cd C:\Users\erick\Documents\Github\testSiefi\SIEFI\
+cd C:\Users\gabri\OneDrive\Documents\GitHub\SIEFI\
 @REM Actualizar repositorio de git | Opcional
-git fetch origin
+@REM git fetch origin
 @REM Instalar todas las dependencias de node
 @REM ! Se cierra despues de que se instalan las dependencias
 @REM npm install
 @REM Limpiar la consola
 cls
 @REM Ejecutar servidor modo silencioso
-nodemon --quiet index
+npm run start_server_hidden
